87b08d1d7c15b44875f93c0753ed4a4882de73a8
Abhisha Bhattacharyya <abhatt22@ncsu.edu>
2019-02-13 15:46:33 -0500

    Adding some new Capabilities

--- src/main/java/com/bigchaindb/smartchaindb/driver/Capabilities.java
+++ src/main/java/com/bigchaindb/smartchaindb/driver/Capabilities.java
@@ -1,5 +1,7 @@
 package com.bigchaindb.smartchaindb.driver;
 
 public interface Capabilities {
-	 public static String PLASTIC = "plastic_manufacturing";
+	public static String PLASTIC = "Plastics_Manufacturing";
+	public static String 3DPRINTING = "3D_Printing";
+	public static String MISC = "Miscellaneous";
 }
