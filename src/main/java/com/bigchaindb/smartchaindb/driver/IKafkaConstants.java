284f0d265b65be313829e9ca3f80208997b2d711
Akash-Pateria <apateri@ncsu.edu>
2020-07-21 21:56:38 -0400

    Change broker IP

--- src/main/java/com/bigchaindb/smartchaindb/driver/IKafkaConstants.java
+++ src/main/java/com/bigchaindb/smartchaindb/driver/IKafkaConstants.java
@@ -1,7 +1,7 @@
 package com.bigchaindb.smartchaindb.driver;
 
 public interface IKafkaConstants {
-    public static String KAFKA_BROKERS = "152.7.99.26:9092";
+    public static String KAFKA_BROKERS = "152.46.17.69:9092";
     public static Integer MAX_NO_MESSAGE_FOUND_COUNT = 300;
     public static String OFFSET_RESET_LATEST = "latest";
     public static String OFFSET_RESET_EARLIER = "earliest";
